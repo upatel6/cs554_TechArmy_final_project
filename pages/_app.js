4398538e5cd5a708c6875077624dc2d1d2639fde
bhargav-baleja <bhargav.baleja.11@gmail.com>
2022-05-14 15:53:48 -0400

    Added everything related pages and its features

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,41 @@
-import '../styles/globals.css'
+import App, { Container } from "next/app";
+import React, { Fragment } from "react";
+import withReduxStore from "../client/redux/store";
+import { Provider } from "react-redux";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MyApp extends App {
+  render() {
+    const { Component, pageProps, reduxStore } = this.props;
+    return (
+      <Fragment>
+        <Container>
+          <Provider store={reduxStore}>
+            <Component {...pageProps} />
+          </Provider>
+        </Container>
+        {/* <style global jsx>{`
+          * {
+            margin: 0;
+            padding: 0;
+            text-decoration: 0;
+            list-style: 0;
+          }
+
+          html,
+          body,
+          #_next {
+            height: 100%;
+            width: 100%;
+          }
+          body {
+            //background-color: #444;
+            color: rgba(256, 256, 256, 0.7);
+            font-weight: 300;
+          }
+        `}</style> */}
+      </Fragment>
+    );
+  }
 }
 
-export default MyApp
+export default withReduxStore(MyApp);
---
+++ pages/_document.js
@@ -0,0 +1,52 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link
+            rel="stylesheet"
+            href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+            integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="stylesheet"
+            href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+            integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay"
+            crossOrigin="anonymous"
+          />
+          <script
+            defer
+            src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+            integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+            crossOrigin="anonymous"
+          />
+          <script
+            defer
+            src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+            integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+            crossOrigin="anonymous"
+          />
+          <script
+            defer
+            src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+            integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+            crossOrigin="anonymous"
+          />
+          <script
+            defer
+            src="https://cdnjs.cloudflare.com/ajax/libs/apexcharts/3.6.12/apexcharts.min.js"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
